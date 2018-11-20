15f3621fe8ea5f6790bb06c9513ae32a8d12dfea
khoursaaf <khoursa@gmail.com>
2018-11-20 16:23:17 +0100

    correction

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Traduire ce code avec des if et des else :</p>
-      <p><code>echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';</code></p>
-      <p>
-        <?php
-        $gender = "Homme";
-        if ($gender = "Homme") {
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Traduire ce code avec des if et des else :</p>
+    <p>echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';</p>
+    <p>
+      <?php
+      $gender = "Homme";
+      if ($gender = "Homme") {
         echo "C'est un développeur !!!";
-        } else {
-          echo "C'est une développeuse !!!";
-        }
-         ?>
-      </p>
-    </body>
+      } else {
+        echo "C'est une développeuse !!!";
+      }
+      ?>
+    </p>
+  </body>
 </html>
